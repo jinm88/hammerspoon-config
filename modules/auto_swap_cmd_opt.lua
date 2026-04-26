8f80f414fc0c6b09d490f0d5f52750bff4b67d6f
majin <jinm1988@gmail.com>
2026-04-26 14:18:44 +0800

    feat: add blacklist and swap logic

--- modules/auto_swap_cmd_opt.lua
+++ modules/auto_swap_cmd_opt.lua
@@ -2,6 +2,45 @@
 -- 蓝牙键盘修饰键自动交换
 -- **************************************************
 
+local M = {}
+
+-- 黑名单配置
+local BLACKLIST = {
+    vendor_ids = {
+        -- Apple
+        ["0x05AC"] = true,
+    },
+    device_names = {
+        -- 例：["Magic Keyboard"] = true,
+    },
+}
+
+-- 检查设备是否在黑名单中
+function M.isBlacklisted(device)
+    if device.vendorID then
+        local vid = string.format("0x%04X", device.vendorID)
+        if BLACKLIST.vendor_ids[vid] then
+            return true
+        end
+    end
+    if device.name then
+        if BLACKLIST.device_names[device.name] then
+            return true
+        end
+    end
+    return false
+end
+
+-- 交换修饰键
+function M.swapKeyboardModifiers()
+    hs.keycodes.masicap(mods_swap)
+end
+
+-- 恢复修饰键
+function M.restoreKeyboardModifiers()
+    hs.keycodes.masicap(mods_swap_flipped)
+end
+
 local mods_swap = {
     left = "cmd",
     right = "opt",
@@ -12,7 +51,4 @@ local mods_swap_flipped = {
     right = "cmd",
 }
 
-return {
-    mods_swap = mods_swap,
-    mods_swap_flipped = mods_swap_flipped,
-}
+return M
